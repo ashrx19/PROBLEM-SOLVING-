e2187a33f351321e87537b46d7c0007890b9ada3
ashrx19 <ashwin191205@gmail.com>
2026-02-02 22:50:24 +0530

    D5P1

---
+++ D5P1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        System.out.println(s.replace(" ", ""));
+    }
+}
+// Problem Statement
+// Write a Java program that reads a string from standard input and removes all spaces from it. The program should then print the modified string to standard output.
