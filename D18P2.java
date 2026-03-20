a9a0001b70e97a9cbd9562c175e49d83e2229f73
ashrx19 <ashwin191205@gmail.com>
2026-03-20 12:00:00 +0530

    D18P2

---
+++ D18P2.java
@@ -0,0 +1,22 @@
+// Print Squares
+// Write a program to output the squares (using multiplication) of numbers from 1 to 5 on separate lines.
+
+// Sample 1:
+// Input
+// Output
+
+// 1 - 1
+// 2 - 4
+// 3 - 9
+// 4 - 16
+// 5 - 25
+
+class Codechef
+{
+    public static void main (String[] args) 
+    {
+        for (int i = 1; i < 6; i++ ){
+            System.out.println(i + "-" + (i * i));
+        }
+    }
+}
