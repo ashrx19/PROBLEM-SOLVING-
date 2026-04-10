a21ea6d6457bab0cffb9e472d308ec41df2f0169
ashrx19 <ashwin191205@gmail.com>
2026-04-11 00:57:40 +0530

    D22P1

---
+++ D22.java
@@ -0,0 +1,31 @@
+// Print the length
+// Write a program to print the length of each word of the sentence given below as well as the length of the whole sentence.
+
+// Note - Make sure to give a space after every word or "-" when printing
+
+// Sample 1:
+// Input
+// Output
+// Coding - 6  
+// on - 2  
+// CodeChef - 8  
+// Coding on CodeChef - 18  
+
+ class Codechef
+{
+	public static void main (String[] args) 
+	{
+		String one = "Coding";
+        String two = "on";
+        String three = "CodeChef";
+        String four ="Coding on CodeChef";
+        
+        
+        System.out.println(one+" - " + one.length());
+        System.out.println(two+" - " + two.length());
+        System.out.println(three+" - " + three.length());
+        System.out.println(four +" - "+ four.length());
+        
+
+	}
+}
