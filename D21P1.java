77bb435f2571d8251a57c5992faeafbf8d6b34e2
ashrx19 <ashwin191205@gmail.com>
2026-04-10 18:00:00 +0530

    D21P1

---
+++ D21P1.java
@@ -0,0 +1,16 @@
+Identify the errors
+Identify and rectify the errors in the code provided to get the desired output.
+
+Sample 1:
+Input
+Output
+Coding Chef
+
+public class Main {
+    public static void main(String[] args) {
+        String one = "Coding";
+        String two = "Chaf";
+        two=two.replace("a","e");
+        System.out.println(one + " " + two);
+    }
+}
