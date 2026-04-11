e1f33aec15038be0717b69ace0c190dfb04e16f6
ashrx19 <ashwin191205@gmail.com>
2026-04-11 12:00:00 +0530

    temp commit

---
+++ D19P2.java
@@ -0,0 +1,16 @@
+class Geeks{
+    public static void main(String[] args) {
+        
+        float si = 5.5f; 
+        
+        int t = 10; 
+        int s = 20; 
+        
+        char var = 'h';               
+
+        System.out.println("Simple Interest: " + si);  
+        System.out.println("Speed: " + s);                      
+        System.out.println("Time: " + t);                        
+        System.out.println("Character: " + var);                
+    }
+}
