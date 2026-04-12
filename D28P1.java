a42ac477ea8604809e73814885c1537adf4254f6
ashrx19 <ashwin191205@gmail.com>
2026-04-12 18:30:00 +0530

    D28P1

---
+++ D28P1.java
@@ -0,0 +1,72 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		// write your code here
+		System.out.println( (n*n)/2 );
+		
+	}
+}
+
+
+// Black cells in a chessboard
+// Read problem statements in Mandarin Chinese and Bengali.
+// Given 
+// n
+// n (
+// n
+// n is even), determine the number of black cells in an 
+// n
+// ×
+// n
+// n×n chessboard.
+
+// Input Format
+// The only line of the input contains a single integer 
+// n
+// n.
+
+// Output Format
+// Output the number of black cells in an 
+// n
+// ×
+// n
+// n×n chessboard.
+
+// Constraints
+// 2
+// ≤
+// n
+// ≤
+// 100
+// 2≤n≤100
+// n
+// n is even
+// Sample 1:
+// Input
+// Output
+// 8
+// 32
+// Explanation:
+
+// There are 
+// 32
+// 32 black cells and 
+// 32
+// 32 white cells in an 
+// 8
+// ×
+// 8
+// 8×8 chessboard. So the answer is 
+// 32
+// 32.
+
+// Did you like the problem?
+// 70 users found this helpful
