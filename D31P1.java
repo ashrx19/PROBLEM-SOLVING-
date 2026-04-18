39e6e2640680b8920c7dfaa8528f36af33d43655
ashrx19 <ashwin191205@gmail.com>
2026-04-19 00:33:22 +0530

    D31P1

---
+++ D31P1.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+class Geeks {
+  
+      // Main Function
+    public static void main (String[] args) {
+      
+        // Logical operators
+        boolean x = true;
+        boolean y = false;
+      
+        System.out.println("x && y: " + (x && y));
+        System.out.println("x || y: " + (x || y));
+        System.out.println("!x: " + (!x));
+    }
+}
