af0caf5e264bfa8b4215267394e7ff0b07449b98
ashrx19 <ashwin191205@gmail.com>
2026-04-29 18:30:00 +0530

    D39P2

--- D19P1.java
+++ D19P1.java
@@ -59,5 +59,5 @@ class DoNotTerminate {
 
 // 100
 // Sample Output 0
-//GIT_AUTHOR_DATE="2026-05-02 18:30:00" GIT_COMMITTER_DATE="2026-05-02 18:30:00" git commit -m "D40P1"
+//GIT_AUTHOR_DATE="2026-05-08 18:30:00" GIT_COMMITTER_DATE="2026-05-08" 18:30:00" git commit -m "D39P2"
 // // Good job
