cbc7848c5a45e3dadf54ada74e9814c11cfdf701
ashrx19 <ashwin191205@gmail.com>
2026-06-03 23:24:18 +0530

    D44P2

---
+++ D44P2.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		 Scanner scanner = new Scanner(System.in);
+        
+        // Read the number of test cases
+        if (scanner.hasNextInt()) {
+            int t = scanner.nextInt();
+            
+            // Loop through each test case
+            while (t-- > 0) {
+                int N = scanner.nextInt();
+                // Print the result for each test case
+                System.out.println(N + 1);
+            }
+        }
+        scanner.close();
+
+	}
+}
+import java.util.Scanner;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		 Scanner scanner = new Scanner(System.in);
+        
+        // Read the number of test cases
+        if (scanner.hasNextInt()) {
+            int t = scanner.nextInt();
+            
+            // Loop through each test case
+            while (t-- > 0) {
+                int N = scanner.nextInt();
+                // Print the result for each test case
+                System.out.println(N + 1);
+            }
+        }
+        scanner.close();
+
+	}
+}
