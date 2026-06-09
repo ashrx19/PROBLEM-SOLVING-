648778fa84196142dea1c357f7e6f19a6c422110
ashrx19 <ashwin191205@gmail.com>
2026-06-09 23:29:09 +0530

    D47P1

---
+++ D47P1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+        Scanner read = new Scanner(System.in);
+        // N should be an int
+        int N = read.nextInt();
+        // Use next() to read the string
+        String S = read.next();
+        // Add a space " " between N and S
+		System.out.print(N + " ");
+		System.out.print(S);
+	}
+}
